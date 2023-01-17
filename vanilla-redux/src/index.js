05f90d15a9c915ea2ae2b901954628fcd97ffb00
“nimoseel” <fromsomini@gmail.com>
2023-01-17 13:13:33 +0900

    리덕스 사용

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,22 +1,38 @@
+import { createStore } from "redux";
+
 const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
+number.innerText = 0;
 
-const updateText = () => {
-  number.innerText = count;
+const PLUS = "PLUS";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => { // 유일하게 데이터를 바꿀 수 있는 곳
+  switch (action.type){
+    case PLUS:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
 }
 
-const handlePlus = () => {
-  count = count + 1;
-  updateText();
-}
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-}
+countStore.subscribe(onChange);
 
-plus.addEventListener("click", handlePlus)
-minus.addEventListener("click", handleMinus)
+plus.addEventListener('click', () => {
+  countStore.dispatch({ type : PLUS });
+})
+
+minus.addEventListener('click', () => { 
+  countStore.dispatch({ type : MINUS }); 
+  // "MINUS" 문자열로 작성할 경우 에러를 잡아주지 않지만 변수를 사용하면 오타를 작성했을 경우 에러를 잡아줌
+})
