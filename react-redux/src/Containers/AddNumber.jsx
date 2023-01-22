406824c6780d2da815ff333b05d4bb437332e05b
“nimoseel” <fromsomini@gmail.com>
2023-01-22 14:13:17 +0900

    connect 사용

--- react-redux/src/Containers/AddNumber.jsx
+++ react-redux/src/Containers/AddNumber.jsx
@@ -1,14 +1,28 @@
-import React, { Component } from "react";
 import AddNumber from "../Components/AddNumber";
-import store from "../store";
+import { connect } from "react-redux";
 
-//container component
-export default class extends Component{
-    render(){
-        return (
-            <AddNumber onClick={function(size){
-                store.dispatch({type:'INCREMENT', size: size})
-            }.bind(this)}></AddNumber>
-        )
+function mapReduxDispatchToReactProps(dispatch){
+    // 리덕스의 dispatch를 리액트 컴포넌트의 props로 연결
+    // 첫번째 인자로 store.dispatch api 함수를 공급
+    return {
+        onClick: function(size){
+            dispatch({type:'INCREMENT', size: size})
+        }
     }
 }
+
+// 이벤트 프롭스만 전달하고 상태전달하는 프롭스 없기 때문에 
+export default connect(null, mapReduxDispatchToReactProps)(AddNumber);
+
+//container component
+// import React, { Component } from "react";
+// import store from "../store";
+// export default class extends Component{
+//     render(){
+//         return (
+//             <AddNumber onClick={function(size){
+//                 store.dispatch({type:'INCREMENT', size: size})
+//             }.bind(this)}></AddNumber>
+//         )
+//     }
+// }
--- react-redux/src/Containers/DisplayNumber.jsx
+++ react-redux/src/Containers/DisplayNumber.jsx
@@ -1,20 +1,32 @@
 import DisplayNumber from "../Components/DisplayNumber";
-import React, { Component } from "react";
-import store from "../store";
+import { connect } from "react-redux";
 
-//container component
-export default class extends Component{
-    state = { number : store.getState().number }
-    constructor(props){
-        super(props);
-        store.subscribe(function(){
-            this.setState({number:store.getState().number});
-        }.bind(this)) 
-        // store의 값이 바뀌면 콜백함수가 호출되면서 state값 세팅해줌
-    }
-    render(){
-        return(
-            <DisplayNumber number={this.state.number}></DisplayNumber>
-        )
+function mapReduxStateToReactProps(state){
+// 리덕스의 store의 state를 리액트의 props로 맵핑
+    return {
+        // 객체의 이름은 전달하고 싶은 props의 이름 = number
+        number: state.number
     }
 }
+
+export default connect(mapReduxStateToReactProps)(DisplayNumber); 
+// connect 리턴값은 함수, 리턴된 함수를 실행하여 만들어진 값을 export
+
+// import React, { Component } from "react";
+// import store from "../store";
+// //container component
+// export default class extends Component{
+//     state = { number : store.getState().number }
+//     constructor(props){
+//         super(props);
+//         store.subscribe(function(){
+//             this.setState({number:store.getState().number});
+//         }.bind(this)) 
+//         // store의 값이 바뀌면 콜백함수가 호출되면서 state값 세팅해줌
+//     }
+//     render(){
+//         return(
+//             <DisplayNumber number={this.state.number}></DisplayNumber>
+//         )
+//     }
+// }
