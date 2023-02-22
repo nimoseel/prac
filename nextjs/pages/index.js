3fbbecb1cb1ef67e7ee0ec9d1ce51519b2584662
“nimoseel” <fromsomini@gmail.com>
2023-02-22 15:13:55 +0900

    getServerSideProps 함수

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,24 +1,12 @@
 
 import Link from "next/link";
 import Seo from "@/components/Seo"
-import { useEffect, useState } from "react";
 
-export default function Home(){
-    const [ movies, setMovies ] = useState();
-    useEffect(()=>{
-        (async()=>{
-            const { results } = await (await fetch(
-                `/api/movies`
-            )).json();
-                setMovies(results);
-        })();
-    },[]);
-
+export default function Home({results}){
     return (
         <div className="container">
             <Seo title='Home'/>
-            {!movies && <h4>Loading...</h4>}
-            {movies?.map((movie) => (
+            {results?.map((movie) => (
                 <div key={movie.id} className="movie">
                     <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
                     <h4>
@@ -57,3 +45,21 @@ export default function Home(){
         </div>
     )
 } 
+
+export async function getServerSideProps(){ 
+    // getServerSideProps 함수 작성 전 
+    // 사전 생성된 html 페이지에 데이터가 포함되어 있지 않았음, (Next.js는 페이지를 미리 html로 export해줌)
+    // React.js 의 처리가 완료될 때까지 기다려야하고, 유저는 api에서 데이터를 받아올 때까지 로딩중 화면을 봐야했음
+
+    // getServerSideProps는 페이지가 유저에게 보여지기 전에 props를 받아오는 함수, 
+    // 즉 데이터가 모두 준비되었을 때 유저들에게 페이지가 보여지게 됨. 
+    // 이 곳에 작성한 코드는 server에서 돌아가게 되기 때문에 
+    // rewrite를 통해 api_key를 숨기지 않고 이곳에 작성해도 client에겐 보여지지 않음 
+    const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+    return {
+        props:{ // props안에 우리의 result가 들어감
+            results,
+        }
+    }
+}
+// Next.js는 getServerSideProps를 실행시켜서 _app.js의 pageProps에 넣어줌
